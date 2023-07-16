08ff7c131f5175738f81c23fc4de2263fd20758d
NolvertoUrias <nuriasobeso@gmail.com>
2023-07-16 14:37:13 -0700

    Adding data binding step by step

--- app/src/main/java/nou/com/example/databindingaboutme/MainActivity.kt
+++ app/src/main/java/nou/com/example/databindingaboutme/MainActivity.kt
@@ -10,18 +10,25 @@ import nou.com.example.databindingaboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // TODO 3: Create a binding object in the MainActivity
     private lateinit var binding: ActivityMainBinding
+
+    // TODO 10: Create instance myName
     private val myName: MyName = MyName("Nolverto Urias")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
+        // TODO 4: In onCreate(), use DataBindingUtil to set the layout for MainActivity
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 //        binding = ActivityMainBinding.inflate(layoutInflater)
 //        setContentView(binding.root)
 
+
+        // TODO 11: Set binding to instance myName
         binding.myName = myName
 
+        // TODO 5: Use the binding object to replace all calls to findViewId
         binding.doneButton.setOnClickListener {
             addNickname(it)
         }
@@ -31,6 +38,7 @@ class MainActivity : AppCompatActivity() {
     private fun addNickname(view: View){
         // Use binding.apply to make the code easier to read
         binding.apply {
+            // TODO 12: Set the nickname in the binding object and invalidateAll()
             myName?.nickname = nicknameEdit.text.toString() // Gets the input from editText using dataBinding
             invalidateAll()                                 // Refresh UI with the new data
             nicknameEdit.visibility = View.GONE             // Hides the editText element
--- app/src/main/java/nou/com/example/databindingaboutme/MyName.kt
+++ app/src/main/java/nou/com/example/databindingaboutme/MyName.kt
@@ -1,5 +1,6 @@
 package nou.com.example.databindingaboutme
 
+// TODO 6: Create a new data class MyName for the name and nickname
 data class MyName(
     var name    : String = "",
     var nickname: String = ""
